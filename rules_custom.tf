ac819680d85b4acafc8672516befc54862184c84
Bence Bánó <18106157+Ledermayer@users.noreply.github.com>
2024-03-22 02:42:52 +0100

    nokey

--- rules_custom.tf
+++ rules_custom.tf
@@ -14,7 +14,7 @@ resource "azurerm_network_security_rule" "custom_rules" {
 
   #General
   resource_group_name         = var.resource_group_object.name
-  network_security_group_name = coalesce(azurerm_network_security_group.nsg["nsg"].name, var.nsg_name)
+  network_security_group_name = coalesce(azurerm_network_security_group.nsg.name, var.nsg_name)
   name                        = each.value.name
   description                 = coalesce(each.value.description, "Security rule for ${each.value.name}")
   priority                    = each.value.priority
